bbd9b961f05a2801629ab102f230a4a8a1eea103
eunji <67421511+eunji1002@users.noreply.github.com>
2024-06-11 23:44:52 +0900

    Update startup_pbspro_exporter.sh

--- docker/startup_pbspro_exporter.sh
+++ docker/startup_pbspro_exporter.sh
@@ -11,6 +11,6 @@ docker run \
 	--name $CNAME \
 	--network host \
 	--restart always \
-	-e PBS_ADDR=34.64.188.40 \
+	-e PBS_ADDR=172.18.7.10 \
 	-e EXPORTER_PORT=9107 \
 	-d $IMGPRJ/$IMGNAME:$IMGTAG
