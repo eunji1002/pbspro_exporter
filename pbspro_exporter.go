31c18ae8b263bcbc598a3cfc9ee3233d6659ba9b
eunji <67421511+eunji1002@users.noreply.github.com>
2024-06-11 14:52:27 +0900

    Update pbspro_exporter.go

--- pbspro_exporter.go
+++ pbspro_exporter.go
@@ -23,7 +23,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/log"
 	"github.com/prometheus/common/version"
-	"github.com/paratera/pbspro_exporter/collector"
+	"github.com/eunji1002/pbspro_exporter/collector"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
