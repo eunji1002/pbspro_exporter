0ff2c461c700d081baa32d1a04d97fb06a49d711
eunji <67421511+eunji1002@users.noreply.github.com>
2024-06-11 21:45:02 +0900

    Update pbspro_exporter.go

--- pbspro_exporter.go
+++ pbspro_exporter.go
@@ -116,7 +116,7 @@ func (h *handler) innerHandler(filters ...string) (http.Handler, error) {
 	handler := promhttp.HandlerFor(
 		prometheus.Gatherers{h.exporterMetricsRegistry, r},
 		promhttp.HandlerOpts{
-			ErrorLog:            promlog.NewErrorLogger(),
+			ErrorLog:            promhttp.NewErrorLogger(),
 			ErrorHandling:       promhttp.ContinueOnError,
 			MaxRequestsInFlight: h.maxRequests,
 		},
