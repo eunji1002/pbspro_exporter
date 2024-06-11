5168a95c4098baeb52b8d5ce2b03b681f4c612c7
eunji <67421511+eunji1002@users.noreply.github.com>
2024-06-11 18:13:10 +0900

    Update pbspro_exporter.go

--- pbspro_exporter.go
+++ pbspro_exporter.go
@@ -107,17 +107,7 @@ func (h *handler) innerHandler(filters ...string) (http.Handler, error) {
 	}
 
 	r := prometheus.NewRegistry()
-	r.MustRegister(version.Print("pbspro_exporter"))
-	versionMetric := prometheus.NewGaugeVec(
-    	    prometheus.GaugeOpts{
-                Name: "pbspro_exporter_build_info",
-                Help: "A metric with a constant '1' value labeled by version, revision, branch, and goversion from which pbspro_exporter was built.",
-    	    },
-    	    []string{"version", "revision", "branch", "goversion"},
-)
-	versionMetric.WithLabelValues(versionInfo).Set(1)
-	r.MustRegister(versionMetric)
-	
+	r.MustRegister(version.BuildContext("pbspro_exporter"))
 	if err := r.Register(nc); err != nil {
 		return nil, fmt.Errorf("couldn't register pbspro collector: %s", err)
 	}
