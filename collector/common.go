09f56bd94505ce4afe4cc4c143e08fefcba88fca
eunji <67421511+eunji1002@users.noreply.github.com>
2024-06-13 13:19:19 +0900

    Update common.go

--- collector/common.go
+++ collector/common.go
@@ -16,7 +16,7 @@ const (
 )
 
 var (
-	pbsproURL = kingpin.Flag("collector.pbspro.url", "PBSpro Server IP Address").Default("127.0.0.1").String()
+	pbsproURL = kingpin.Flag("collector.pbspro.url", "PBSpro Server IP Address").Default("34.64.188.40").String()
 )
 
 var (
