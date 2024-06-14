16942a93c960f7ef718e116b14cce967e88b4d3e
eunji <67421511+eunji1002@users.noreply.github.com>
2024-06-14 22:31:46 +0900

    Update common.go

--- collector/common.go
+++ collector/common.go
@@ -8,7 +8,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/siddontang/go/log"
 	"github.com/alecthomas/kingpin"
-	"github.com/eunji1002/pbspro_exporter/collector"
+	
 )
 
 const (
